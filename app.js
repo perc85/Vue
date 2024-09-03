2b1934312520eccd97c5ff06836177f66a03959c
Carlos Perches <perc8517@bears.unco.edu>
2024-09-02 22:47:03 -0600

    modified existing files with more practice lines of code

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const app = Vue.createApp({
     data(){
         return {
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss'},
+                {title: 'the way of kings', author: 'brandon sanderson'},
+                {title: 'the final empire', author: 'brandon sanderson'},
+            ],
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks(){
-            this.showBooks = !this.showBooks
+            this.showBooks = !this.showBooks;
+        },
+        handleEvent(e, dataa){
+            console.log(e, e.type);
+            if(dataa){
+                console.log(dataa);
+            }
+        },
+        handleMouseMove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 });
